0e21cc26db2841ebfc2f5c425226c7ba7ae10076
ukaracivio <umit.karacivi@outlook.com>
2023-12-25 11:44:56 +0300

    Some changes

---
+++ eTickets_Web/Enums/MovieCategory.cs
@@ -0,0 +1,17 @@
+﻿namespace eTickets_Web.Enums
+{
+    public enum MovieCategory
+    {
+        // Mopvie kategorileri listesi enum yapısı şeklinde tutulacak
+
+        // normalde enum yapısı index değerini 0 dan tutar ..ama ben 1 den başlamasını istiyorum ki ilişkilendirmelere girecek
+
+        Action=1,
+        Comedy,
+        Drama,
+        Documentary,
+        Cartoon,
+        Horror
+
+    }
+}
---
+++ eTickets_Web/Models/Movie.cs
@@ -0,0 +1,31 @@
+﻿using eTickets_Web.Base;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets_Web.Models
+{
+    public class Movie : IEntityBase
+    {
+        [Key]
+        public int Id { get ; set ; }
+
+        [Display(Name = "Movie Name")] // Viewlardaki ilgili alanın başındaki görünecek text
+        public string Name { get; set; } // Cinema adı
+
+        [Display(Name = "Description")] // Viewlardaki ilgili alanın başındaki görünecek text
+        public string Description { get; set; }
+
+        [Display(Name = "Ticket Price")] // Viewlardaki ilgili alanın başındaki görünecek text
+        public double Price { get; set; }
+
+        [Display(Name = "Affiche")] // Viewlardaki ilgili alanın başındaki görünecek text
+        public string ImageURL { get; set; }
+
+        [Display(Name = "Start Date")] // Viewlardaki ilgili alanın başındaki görünecek text
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "End Date")] // Viewlardaki ilgili alanın başındaki görünecek text
+        public DateTime EndDate  { get; set; }
+
+        //Eklenecekler var
+    }
+}
