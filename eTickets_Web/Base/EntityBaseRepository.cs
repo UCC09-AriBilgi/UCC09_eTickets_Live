6e4584daadfc98bb5953a6fd7eaeb96c995c9de4
ukaracivio <umit.karacivi@outlook.com>
2023-12-26 14:46:02 +0300

    Some changes

--- eTickets_Web/Base/EntityBaseRepository.cs
+++ eTickets_Web/Base/EntityBaseRepository.cs
@@ -1,4 +1,5 @@
 ﻿using eTickets_Web.Data;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System.Linq.Expressions;
 
 namespace eTickets_Web.Base
@@ -6,23 +7,39 @@ namespace eTickets_Web.Base
     // Genel CRUD metotlarını buraya toplayacağız
     public class EntityBaseRepository<T> : IEntityBaseRepository<T> where T : class, IEntityBase, new()
     {
+        private readonly AppDbContext _context;
+
         public EntityBaseRepository(AppDbContext context)
         {
+            _context = context;
         }
 
         public void Add(T entity)
         {
-            throw new NotImplementedException();
+            // Üzerine gelen T Type buradaki modelim
+            _context.Set<T>().Add(entity); // Hangi model için o an işlem yapacağını öğrenebilmesi ve işlemini yapabilmesi için 
+
+            _context.SaveChanges(); // Db tarafına gönderim için
+            
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            var entity=_context.Set<T>().FirstOrDefault(x => x.Id == id);
+
+            EntityEntry entityEntry = _context.Entry<T>(entity);
+
+            entityEntry.State = Microsoft.EntityFrameworkCore.EntityState.Deleted; // Kaydımı siliyor
+
+            _context.SaveChanges();
+
         }
 
         public IEnumerable<T> GetAll()
         {
-            throw new NotImplementedException();
+            var result=_context.Set<T>().ToList();
+
+            return result;
         }
 
         public IEnumerable<T> GetAll(params Expression<Func<T, object>>[] includeProperties)
