330e2ea46d61bf95ab764a8488b573b08299ff53
ukaracivio <umit.karacivi@outlook.com>
2023-12-26 14:05:50 +0300

    Some changes

--- eTickets_Web/Program.cs
+++ eTickets_Web/Program.cs
@@ -6,55 +6,65 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
 
-// DbContext Configuration
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"))); // appsettings.json daki Conn tanýmýný al
+        // DbContext Configuration
+        builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"))); // appsettings.json daki Conn tanýmýný al
 
-// Service Configuration
-// Scoped (Kapsamlý) : Her service kapsamý için yeni bir instance(örnek) oluþturulur.
-// peakup.org
-//builder.Services.AddScoped<IActorsService, ActorsService>();
-//builder.Services.AddScoped<IProducersService, ProducersService>();
-//builder.Services.AddScoped<ICinemasService, CinemasService>();
-//builder.Services.AddScoped<IMoviesService, MoviesService>();
+        // Service Configuration
+        // Scoped (Kapsamlý) : Her service kapsamý için yeni bir instance(örnek) oluþturulur.
+        // peakup.org
+        //builder.Services.AddScoped<IActorsService, ActorsService>();
+        //builder.Services.AddScoped<IProducersService, ProducersService>();
+        //builder.Services.AddScoped<ICinemasService, CinemasService>();
+        //builder.Services.AddScoped<IMoviesService, MoviesService>();
 
-// Authentication and Authorization
-builder.Services.AddIdentity<ApplicationUser,IdentityRole>()
-    .AddEntityFrameworkStores<AppDbContext>();
-builder.Services.AddMemoryCache();
-builder.Services.AddSession();
-builder.Services.AddAuthorization(options => 
-        {
-            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        // Authentication and Authorization
+        builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>();
+        builder.Services.AddMemoryCache();
+        builder.Services.AddSession();
+        builder.Services.AddAuthorization(options => {
+            //options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 
         });
 
 
-//
+        //
 
-var app = builder.Build();
+        var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        // Seeding Database - Örnek datalarý oluþturulmasý
+
+        AppDbInitializer.Seed(app); // user data
+        //AppDbInitializer.SeedUsersAndRolesAsync(app).Wait(); // user auth ????????
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
