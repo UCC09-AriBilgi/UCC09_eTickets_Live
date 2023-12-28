9146d8dbdf680c1697d930b5ea07c7978edce7ac
ukaracivio <umit.karacivi@outlook.com>
2023-12-28 12:17:21 +0300

    Some changes

---
+++ eTickets_Web/Controllers/AccountController.cs
@@ -0,0 +1,109 @@
+﻿using eTickets_Web.Data;
+using eTickets_Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eTickets_Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signinManager;
+        private readonly AppDbContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signinManager, AppDbContext context)
+        {
+            _userManager = userManager;  
+            _signinManager = signinManager;  
+            _context = context;  
+        }
+
+        // Login kısmı
+        public IActionResult Login()
+        {
+            var response = new LoginVM();
+
+            return View(response);
+        }
+
+
+
+
+
+        // GET: AccountController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AccountController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AccountController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AccountController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AccountController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AccountController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ eTickets_Web/Data/ViewModels/LoginVM.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets_Web.Data.ViewModels
+{
+    public class LoginVM
+    {
+        [Display(Name ="EMail Address")]
+        [Required(ErrorMessage = "EMail adresi gereklidir....")]
+        public string EMailAddress { get; set; }
+
+        [Display(Name = "Password")]
+        [Required(ErrorMessage ="Şifre gereklidir...")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
