e38e0de66b3aeb0bba83da1363113a153a45fb47
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-01-02 14:40:17 +0300

    Some changes

---
+++ eTickets_Web/Data/ViewModels/NewMovieDropdownsVM.cs
@@ -0,0 +1,24 @@
+﻿using eTickets_Web.Models;
+
+namespace eTickets_Web.Data.ViewModels
+{
+    // Kullanılacak dropdownlar
+    public class NewMovieDropdownsVM
+    {
+        // Değişkenler
+        public List<Producer> Producers { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+        public List<Actor> Actors { get; set; }
+
+        // Constructor
+        public NewMovieDropdownsVM() 
+        { 
+            // Select List lerde kullanacağım listeler
+
+            Producers = new List<Producer>();
+            Cinemas = new List<Cinema>();
+            Actors = new List<Actor>();
+        }    
+
+    }
+}
